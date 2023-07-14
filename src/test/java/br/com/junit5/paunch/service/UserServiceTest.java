b703217d8c3f0a85ffe6f6c02d981c886c603c8c
Danilo N. B. <danilobenatti@gmail.com>
2023-07-14 14:56:57 -0300

    Must return a user by email

--- src/test/java/br/com/junit5/paunch/service/UserServiceTest.java
+++ src/test/java/br/com/junit5/paunch/service/UserServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import br.com.junit5.paunch.domain.User;
+import br.com.junit5.paunch.domain.builder.UserBuilder;
 import br.com.junit5.paunch.service.repository.UserRepository;
 
 class UserServiceTest {
@@ -14,10 +15,28 @@ class UserServiceTest {
 	private UserService service;
 	
 	@Test
-	void mustSaveUserWithSuccess() {
+	void mustReturnUserByEmail() {
 		UserRepository repository = Mockito.mock(UserRepository.class);
 		service = new UserService(repository);
-		Optional<User> user = service.getUserByEmail("user@mail.com");
+		
+		Mockito.when(repository.getUserByEmail("usermail@email.com"))
+			.thenReturn(Optional.of(UserBuilder.oneUser().now()));
+		
+		Optional<User> user = service.getUserByEmail("usermail@email.com");
+		Assertions.assertFalse(user.isEmpty());
+		System.out.println(user);
+	}
+	
+	@Test
+	void mustReturnEmptyWhenUserNotExists() {
+		UserRepository repository = Mockito.mock(UserRepository.class);
+		service = new UserService(repository);
+		
+		Mockito.when(repository.getUserByEmail("usermail@email.com"))
+			.thenReturn(Optional.empty());
+		
+		Optional<User> user = service.getUserByEmail("usermail@email.com");
 		Assertions.assertTrue(user.isEmpty());
 	}
+	
 }
