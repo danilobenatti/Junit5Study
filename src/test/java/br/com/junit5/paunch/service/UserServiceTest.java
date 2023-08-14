5d03d4d1fd61ea4d89703a9dd5f41d9c834190d4
Danilo N. B. <danilobenatti@gmail.com>
2023-08-13 21:40:17 -0300

    Making checks with 'mock'

--- src/test/java/br/com/junit5/paunch/service/UserServiceTest.java
+++ src/test/java/br/com/junit5/paunch/service/UserServiceTest.java
@@ -1,8 +1,14 @@
 package br.com.junit5.paunch.service;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
 import java.util.Optional;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -19,12 +25,23 @@ class UserServiceTest {
 		UserRepository repository = Mockito.mock(UserRepository.class);
 		service = new UserService(repository);
 		
-		Mockito.when(repository.getUserByEmail("usermail@email.com"))
+		when(repository.getUserByEmail("usermail@email.com"))
 			.thenReturn(Optional.of(UserBuilder.oneUser().now()));
 		
 		Optional<User> user = service.getUserByEmail("usermail@email.com");
-		Assertions.assertFalse(user.isEmpty());
+		assertTrue(user.isPresent());
 		System.out.println(user);
+		
+		/**
+		 * verify(mock, times(5)).someMethod("was called five times");
+		 * verify(mock, never()).someMethod("was never called");
+		 * verify(mock, atLeastOnce()).someMethod("was called at least once");
+		 * verify(mock, atLeast(2)).someMethod("was called at least twice");
+		 * verify(mock, atMost(3)).someMethod("was called at most 3 times");
+		 */
+		verify(repository, atLeastOnce()).getUserByEmail("usermail@email.com");
+		verify(repository, never()).getUserByEmail("othermail@email.com");
+		verifyNoMoreInteractions(repository);
 	}
 	
 	@Test
@@ -32,11 +49,11 @@ class UserServiceTest {
 		UserRepository repository = Mockito.mock(UserRepository.class);
 		service = new UserService(repository);
 		
-		Mockito.when(repository.getUserByEmail("usermail@email.com"))
+		when(repository.getUserByEmail("usermail@email.com"))
 			.thenReturn(Optional.empty());
 		
 		Optional<User> user = service.getUserByEmail("usermail@email.com");
-		Assertions.assertTrue(user.isEmpty());
+		assertTrue(user.isEmpty());
 	}
 	
 }
