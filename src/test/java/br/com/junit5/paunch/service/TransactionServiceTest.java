9bee5dd1de1ef7569985523e9d599ae5b92b3ff3
Danilo N. B. <danilobenatti@gmail.com>
2023-12-30 16:50:00 -0300

    Must reject an incomplete transaction

--- src/test/java/br/com/junit5/paunch/service/TransactionServiceTest.java
+++ src/test/java/br/com/junit5/paunch/service/TransactionServiceTest.java
@@ -1,18 +1,28 @@
 package br.com.junit5.paunch.service;
 
+import static br.com.junit5.paunch.domain.builder.AccountBuilder.oneAccount;
 import static br.com.junit5.paunch.domain.builder.TransactionBuilder.oneTransaction;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.junit5.paunch.domain.Account;
 import br.com.junit5.paunch.domain.Transaction;
+import br.com.junit5.paunch.domain.exceptions.ValidationExceptions;
 import br.com.junit5.paunch.service.repository.dao.TransactionDao;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,5 +60,32 @@ class TransactionServiceTest {
 											transactionSaved.getAccount()
 													.getUser().getPassword())));
 				});
+	}
+	
+	@ParameterizedTest(name = "{6}")
+	@MethodSource(value = "mandatoryScenarios")
+	void mustValidateMandatoryFieldsWhenSaving(Long id, String description,
+			Double value, LocalDate date, Account account, Boolean status,
+			String message) {
+		String exMessage = assertThrows(ValidationExceptions.class, () -> {
+			Transaction transaction = oneTransaction().withId(id)
+					.withDescription(description).withValue(value)
+					.withDate(date).withStatus(status).withAccount(account)
+					.now();
+			service.save(transaction);
+		}).getMessage();
+		Assertions.assertEquals(message, exMessage);
+	}
+	
+	static Stream<Arguments> mandatoryScenarios() {
+		return Stream.of(
+				Arguments.of(1L, null, 10D, LocalDate.now(), oneAccount().now(),
+						true, "Description non-existent"),
+				Arguments.of(1L, "Description", null, LocalDate.now(),
+						oneAccount().now(), true, "Value non-existent"),
+				Arguments.of(1L, "Description", 10D, null, oneAccount().now(),
+						true, "Date non-existent"),
+				Arguments.of(1L, "Description", 10D, LocalDate.now(), null,
+						true, "Account non-existent"));
 	}
 }
